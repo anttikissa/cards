cc4b2b6267352eb62a148d2c1b01ac06f41554d2
Antti Sykäri <antti.sykari@gmail.com>
2015-02-27 15:54:57 +0200

    Add placeholder code for saving modified content

--- client/client.js
+++ client/client.js
@@ -1,7 +1,27 @@
 var log = console.log.bind(console);
 
+// $('<div> <p>foo</p> <p>bar</p> </div>') -> 'foo\nbar'
+function textify($el) {
+	return $el.find('p').map(function() {
+		return this.innerHTML;
+	}).get().join('\n');
+}
+
 $(function() {
 	$('body').on('click', '.card .content', function(e) {
-		log('got', e);
+		var $content = $(e.target).closest('.content');
+		$content.data('textWhenFocused', textify($content));
+	});
+
+	$('body').on('blur', '.content', function(e) {
+		var $content = $(e.target);
+		var text = textify($content);
+		var oldText = $content.data('textWhenFocused');
+		if (oldText !== text) {
+			var $card = $content.closest('.card');
+			var id = $card.data('id');
+
+			log('PATCH id', id, 'with text', text);
+		}
 	});
 });
